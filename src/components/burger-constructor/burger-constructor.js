3bac499de80d817eae9dfdcc0e03602f638a512b
KovOlga <switch.to.olga@yandex.ru>
2023-03-21 22:53:57 +0300

    fixed styles for BurgerConstructor

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -7,41 +7,46 @@ import styles from "./burger-constructor.module.css";
 const BurgerConstructor = ({ data }) => {
   return (
     <section className={`${styles.section_constructor} pl-4 pr-4`}>
-      <ul className={styles.list}>
-        <li>
-          <ConstructorElement
-            type="top"
-            isLocked={true}
-            text="Краторная булка N-200i (верх)"
-            price={200}
-            thumbnail={data[0].image}
-          />
-        </li>
-        <div className={`${styles.list} ${styles.list_available}`}>
-          {data.map((item) => {
-            return (
-              <li className={styles.list__item}>
-                <DragIcon type="primary" />
-                {
+      <div className={styles.incridients}>
+        <ConstructorElement
+          extraClass={styles.item__bun}
+          key={"top"}
+          type="top"
+          isLocked={true}
+          text="Краторная булка N-200i (верх)"
+          price={200}
+          thumbnail={data[0].image}
+        />
+
+        <ul className={styles.list}>
+          <>
+            {data.map((item) => {
+              return (
+                <li className={styles.list__item}>
+                  <DragIcon type="primary" />
                   <ConstructorElement
                     key={item._id}
                     text={item.name}
                     price={item.price}
                     thumbnail={item.image}
                   />
-                }
-              </li>
-            );
-          })}
-        </div>
+                </li>
+              );
+            })}
+          </>
+        </ul>
+
         <ConstructorElement
+          extraClass={styles.item__bun}
+          key={"bottom"}
           type="bottom"
           isLocked={true}
           text="Краторная булка N-200i (низ)"
           price={200}
           thumbnail={data[0].image}
         />
-      </ul>
+      </div>
+
       <div className={styles.total}>
         <div className={styles.price}>
           <p className="text text_type_digits-medium">610</p>
