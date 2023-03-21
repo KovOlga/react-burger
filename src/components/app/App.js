a7868fecf890439f5636cfcbd85cac5eedfc2f62
KovOlga <switch.to.olga@yandex.ru>
2023-03-21 12:42:08 +0300

    added data in utils

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 // import styles from "./app.module.css";
 import AppHeader from "../app-header/app-header";
+import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 
 const App = () => {
-  return <>{<AppHeader />}</>;
+  return (
+    <>
+      <AppHeader />
+      <BurgerIngredients />
+    </>
+  );
 };
 
 export default App;
