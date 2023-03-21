a1a23d18efb12a45a533fe02832c6ed8374e2ba6
KovOlga <switch.to.olga@yandex.ru>
2023-03-21 16:13:50 +0300

    added filter for types in list

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -6,6 +6,10 @@ import BurgerIngredientsItem from "../burger-ingredients-item/burger-ingredients
 const BurgerIngredients = ({ data }) => {
   const [current, setCurrent] = React.useState("Булки");
 
+  const bunList = data.filter((item) => item.type === "bun");
+  const sauceList = data.filter((item) => item.type === "sauce");
+  const mainList = data.filter((item) => item.type === "main");
+
   return (
     <section className={styles.burger__ingredients}>
       <h1 className="text text_type_main-large">Соберите бургер</h1>
@@ -29,15 +33,25 @@ const BurgerIngredients = ({ data }) => {
       <div className={styles.ingredients__list}>
         <h2 className="text text_type_main-medium">Булки</h2>
         <ul className={`${styles.list} pl-4 pr-4`}>
-          {data.map((item) => {
+          {bunList.map((item) => {
             const { id, ...itemProps } = item;
             return <BurgerIngredientsItem key={item._id} {...itemProps} />;
           })}
         </ul>
         <h2 className="text text_type_main-medium">Соусы</h2>
-        <ul className={`${styles.list} pl-4 pr-4`}></ul>
+        <ul className={`${styles.list} pl-4 pr-4`}>
+          {sauceList.map((item) => {
+            const { id, ...itemProps } = item;
+            return <BurgerIngredientsItem key={item._id} {...itemProps} />;
+          })}
+        </ul>
         <h2 className="text text_type_main-medium">Начинки</h2>
-        <ul className={`${styles.list} pl-4 pr-4`}></ul>
+        <ul className={`${styles.list} pl-4 pr-4`}>
+          {mainList.map((item) => {
+            const { id, ...itemProps } = item;
+            return <BurgerIngredientsItem key={item._id} {...itemProps} />;
+          })}
+        </ul>
       </div>
     </section>
   );
