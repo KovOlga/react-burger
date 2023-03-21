fc394e6ba2143947f2d221e3b0eb444060078a71
KovOlga <switch.to.olga@yandex.ru>
2023-03-21 16:26:09 +0300

    added counter

--- src/components/burger-ingredients-item/burger-ingredients-item.js
+++ src/components/burger-ingredients-item/burger-ingredients-item.js
@@ -1,11 +1,13 @@
 import styles from "./burger-ingredients-item.module.css";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Counter } from "@ya.praktikum/react-developer-burger-ui-components";
 
 const BurgerIngredientsItem = (props) => {
   const { name, image, price } = props;
 
   return (
     <li className={styles.burger__item}>
+      <Counter count={1} size="default" extraClass="m-1" />
       <img className="pl-4 pr-4" src={image} alt={name}></img>
       <div className={`${styles.price_field} pb-1 pt-1`}>
         <p className="text text_type_digits-default">{price}</p>
--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -12,7 +12,7 @@ const BurgerIngredients = ({ data }) => {
 
   return (
     <section className={styles.burger__ingredients}>
-      <h1 className="text text_type_main-large">Соберите бургер</h1>
+      <h1 className="text text_type_main-large pb-5">Соберите бургер</h1>
 
       <div style={{ display: "flex" }}>
         <Tab value="Булки" active={current === "Булки"} onClick={setCurrent}>
