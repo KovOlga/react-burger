dc22f7fd299bb90b8b1099eb4c6eb05cf8469eef
KovOlga <switch.to.olga@yandex.ru>
2023-03-27 18:07:25 +0300

    added getting data from api

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,51 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import styles from "./app.module.css";
-import data from "../../utils/data";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 
 const App = () => {
+  const baseUrl = "https://norma.nomoreparties.space/api/ingredients";
+  const getResponse = (res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  };
+
+  const [state, setState] = useState({
+    isLoading: false,
+    hasError: false,
+    data: {},
+  });
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    setState({ ...state, hasError: false, isLoading: true });
+    fetch(baseUrl)
+      .then(getResponse)
+      .then(({ data }) => setState({ ...state, data, isLoading: false }))
+      .catch((e) => {
+        setState({ ...state, hasError: true, isLoading: false });
+      });
+  };
+
   return (
     <>
       <AppHeader />
       <main className={styles.main}>
-        <BurgerIngredients data={data} />
-        <BurgerConstructor data={data} />
+        {state.isLoading && "Загрузка..."}
+        {state.hasError && "Произошла ошибка"}
+        {!state.isLoading && !state.hasError && state.data.length && (
+          <>
+            <BurgerIngredients data={state.data} />
+            <BurgerConstructor data={state.data} />
+          </>
+        )}
       </main>
     </>
   );
