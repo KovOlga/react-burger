167eb13170e8a53f851b1630384cbf4aaa1b172d
KovOlga <switch.to.olga@yandex.ru>
2023-07-07 12:14:13 +0300

    added images type

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,3 +12,5 @@ export const LOGIN_ROUTE = "/login";
 export const REGISTER_ROUTE = "/register";
 export const FORGOT_PASSWORD_ROUTE = "/forgot-password";
 export const RESET_PASSWORD_ROUTE = "/reset-password";
+
+export const modalRoot = document.getElementById("react-modals");
